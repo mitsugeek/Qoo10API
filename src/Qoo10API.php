75d6e9a68d5dd3ebddf1a725279e98a83738bb11
mitsugeek <mitsu322@gmail.com>
2021-12-26 11:23:32 +0900

    コンストラクタに販売キー取得を追加

--- src/Qoo10API.php
+++ src/Qoo10API.php
@@ -6,22 +6,65 @@ class Qoo10API
     private $API_KEY;
     private $USER_ID;
     private $USER_PWD;
+    private $HanbaiAPIKey;
 
+    /**
+     * コンストラクタ
+     * 
+     * iniファイル読み込み
+     * 販売認証キー取得
+     */
     function __construct(){
         $ini = parse_ini_file("Qoo10API.ini");
         $this->BASE_DOMAIN = $ini["BASE_DOMAIN"];
         $this->API_KEY = $ini["API_KEY"];
         $this->USER_ID = $ini["USER_ID"];
         $this->USER_PWD = $ini["USER_PWD"];
+        $this->HanbaiAPIKey = $this->CreateCertificationKey();
     }
 
-    // メソッドの宣言
-    public function displayVar() {
+    public function var_dump() {
         var_dump($this);
     }
+
+    /**
+     * API実行用
+     */
+    private function APIExec($param){
+        $queryString = http_build_query($param);
+        $curl = curl_init();
+        $auth_url = "https://".$this->BASE_DOMAIN."/GMKT.INC.Front.QAPIService/ebayjapan.qapi";
+        $auth_url .= "?v=1.0";
+        $auth_url .= "&".$queryString;
+        curl_setopt($curl, CURLOPT_URL, $auth_url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response,TRUE);
+    }
+
+    /**
+     * 販売認証キー発行
+     */
+    private function CreateCertificationKey(){
+        $data = array();
+        $data["returnType"] = "json";
+        $data["method"] = "CertificationAPI.CreateCertificationKey";
+        $data["key"] = $this->API_KEY;
+        $data["user_id"] = $this->USER_ID;
+        $data["pwd"] = $this->USER_PWD;
+        $ret = $this->APIExec($data);
+        if($ret["ResultCode"] == 0){
+          return $ret["ResultObject"];
+        }
+        return "";
+    }
+
 }
 
 if (realpath($_SERVER["SCRIPT_FILENAME"]) == realpath(__FILE__)){
   $test = new Qoo10API();
-  $test->displayVar();
+  $test->var_dump();
 }
